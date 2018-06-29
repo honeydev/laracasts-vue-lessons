3be5304b12877de636a029983b7d2e21cafa0bb7
honey <devspades@gmail.com>
2018-06-29 17:53:24 +1000

    add lessons 9, 10

---
+++ lesson-10/main.js
@@ -0,0 +1,42 @@
+'use strict';
+
+Vue.component('modal', {
+	template: `
+	    <div class="modal is-active">
+		<div class="modal-background"></div>
+          <div class="modal-content">
+            <div class="box">
+            <p>
+                Hello
+                Hello
+                Hello
+                Hello
+                Hello
+                Hello
+                Hello
+                Hello
+                Hello
+                Hello
+                Hello
+                Hello
+                Hello
+                Hello
+                Hello
+                Hello
+                Hello
+                Hello
+            </p>
+            </div>
+          </div>
+          <button class="modal-close is-large" aria-label="close" @click="$emit('close')"></button>
+    	</div>
+	`
+});
+
+new Vue({
+	el: "#root",
+
+	data: {
+		showModal: false
+	}
+})
--- lesson-8/main.js
+++ lesson-8/main.js
@@ -1,7 +1,7 @@
 Vue.component('task-list', {
 	template: `
 		<div>
-			<task v-for="task in tasks">{{ task.task }}</task>
+			<task v-for="tasks in tasks">{{ task.task }}</task>
 		</div>
 		`,	
 	data() {
